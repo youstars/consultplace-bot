cec29d0aeb8508a3a8a0b1a2d127169c04181e2a
youstars <youstars.tech@gmail.com>
2025-06-02 12:22:10 +0200

    test: supply env vars via conftest to satisfy Settings

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("TELEGRAM_TOKEN",   "test-token")
+os.environ.setdefault("BACKEND_BASE_URL", "http://localhost")
+os.environ.setdefault("BACKEND_USER",     "user")
+os.environ.setdefault("BACKEND_PASSWORD", "pass")
+os.environ.setdefault("AI_API_KEY",       "dummy")
